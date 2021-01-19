cf02c2534ad8b94b3ddd15e03e00557db9f286aa
Kevin O'Neill <oneillkeving@gmail.com>
2021-01-19 12:09:36 -0500

    Trying to block learning/mcmc stages

--- exp/exp-accept.js
+++ exp/exp-accept.js
@@ -2,31 +2,57 @@
 var accept_timeline = [];
 var ACCEPT_TRIALS = 0;
 
-/* Display instructions trials */
+/* instructions trials */
+var instructions_pages_accept = [
+	"<p>Please pay attention and carefully read the following instructions.</p><p>In this HIT, you are going to play a game. " +
+	"There are two players: you, and a partner. In every round of this game, you and your partner receive " + stakes_txt + " ($" + stakes.toFixed(2) +
+	"), and you must decide how to split it between the two of you.</p><br><p>Here's how it works:</p>",
+	"<p>First, your partner will offer you some portion of the money.</p><p>They can offer you any amount between $0.00 and $" + stakes.toFixed(2) + ".</p>",
+	"<p>Once your partner makes an offer, you may either accept or reject this offer.</p>" +
+	"<p>If you accept the offer, then you will receive the amount of money offered to you, and your partner will get the rest.</p>" +
+	"<p>If you reject the offer, then nobody gets any money in that round.</p><br>",
+	"<p>Since you will be playing this game many times, you will <b>not</b> receive money from every round of the game.</p>" +
+	"<p><b>But, you will receive the amount of money that you earn from a randomly selected round of the game as a bonus through mTurk. </b>" +
+	"For example, if you accepted an offer of $5.00 on the randomly chosen round, you will receive a bonus of $5.00 after the HIT is completed. " +
+	"But, if you rejected your partner's offer of $5.00, you will receive no bonus at all.</p>" +
+	"<p><b>So, make sure to play each round like real money is up for grabs- you never know which round of the game you will get a bonus from!</b></p>",
+	"<p>In addition to clicking on the buttons, you may use the left and right arrow keys to make quicker responses.</p>",
+	"<img src='img/red_player.png' height='" + stim_size + "'>" +
+	"<img src='img/yellow_player.png' height='" + stim_size + "'>" +
+	"<img src='img/blue_player.png' height='" + stim_size + "'>" +
+	"<p>In each round of the game, " +
+	"you will play with one of three partners: Red, Yellow, or Blue. " +
+	"Before each round, you will be told which partner you are playing with.</p>"];
+
+if (learnBlocks > 1) {
+	instructions_pages_accept.push("<p>One last thing: we want to learn what you think of the different players in this game. " +
+		"So, we'll alternate between playing the game for a few rounds and asking what you think of the players.</p>",
+    "<p>To do so, we will first tell you which partner you are playing with.</p>" +
+    "<p>Then you'll be shown two possible offers, and you will be asked:</p><br><p>\"Which offer is more likely?\"</p><br>" +
+    "<p><b>Please choose the offer amount that your partner is most likely to have offered.</b></p>",
+    "<p>Sometimes it may seem like both offers are equally likely. " +
+    "Even if it's hard to tell, try to pick the offer that feels most likely. " +
+    "You do not need to spend too much time thinking about each choice.</p>");
+}
+
 var instructions_accept = {
   type: 'instructions',
-  pages: ["<p>Please pay attention and carefully read the following instructions.</p><p>In this HIT, you are going to play a game. " +
-    "There are two players: you, and a partner. In every round of this game, you and your partner receive " + stakes_txt + " ($" + stakes.toFixed(2) +
-    "), and you must decide how to split it between the two of you.</p><br><p>Here's how it works:</p>",
-    "<p>First, your partner will offer you some portion of the money.</p><p>They can offer you any amount between $0.00 and $" + stakes.toFixed(2) + ".</p>",
-    "<p>Once your partner makes an offer, you may either accept or reject this offer.</p>" +
-    "<p>If you accept the offer, then you will receive the amount of money offered to you, and your partner will get the rest.</p>" +
-    "<p>If you reject the offer, then nobody gets any money in that round.</p><br>",
-    "<p>Since you will be playing this game many times, you will <b>not</b> receive money from every round of the game.</p>" +
-    "<p><b>But, you will receive the amount of money that you earn from a randomly selected round of the game as a bonus through mTurk. </b>" +
-    "For example, if you accepted an offer of $5.00 on the randomly chosen round, you will receive a bonus of $5.00 after the HIT is completed. " +
-    "But, if you rejected your partner's offer of $5.00, you will receive no bonus at all.</p>" +
-    "<p><b>So, make sure to play each round like real money is up for grabs- you never know which round of the game you will get a bonus from!</b></p>",
-    "<p>In addition to clicking on the buttons, you may use the left and right arrow keys to make quicker responses.</p>",
-    "<img src='img/red_player.png' height='" + stim_size + "'>" +
-    "<img src='img/yellow_player.png' height='" + stim_size + "'>" +
-    "<img src='img/blue_player.png' height='" + stim_size + "'>" +
-    "<p>One more thing about this game: in each round of the game, " +
-    "you will play with one of three partners: Red, Yellow, or Blue. " +
-    "Before each round, you will be told which partner you are playing with.</p>" +
-    "You're now finished with the instructions."],
+  pages: instructions_pages_accept.concat("You're now finished with the instructions."),
   show_clickable_nav: true
 };
+
+var instructions_mcmc_accept = {
+  type: 'instructions',
+  pages: ["<p>Great work!</p><br><p>Now, we want to learn what you think of the different players in this game.</p>",
+          "<p>To do so, we will first tell you which partner you are playing with.</p>" +
+          "<p>Then you'll be shown two possible offers, and you will be asked:</p><br><p>\"Which offer is more likely?\"</p><br>" +
+          "<p><b>Please choose the offer amount that your partner is most likely to have offered.</b></p>",
+          "<p>Sometimes it may seem like both offers are equally likely. " +
+          "Even if it's hard to tell, try to pick the offer that feels most likely. " +
+          "You do not need to spend too much time thinking about each choice.</p>"],
+  show_clickable_nav: true
+};
+
 if (!TEST) {
   accept_timeline.push(instructions_accept);
   ACCEPT_TRIALS = ACCEPT_TRIALS + 1;
@@ -122,7 +148,9 @@ var accept_feedback = {
 };
 
 // learning trials
-function accept_trial(params, stage, cause=false) {
+function accept_learn_trial(n, stage, cause=false) {
+	let params = learnParams(n);
+
   let UGtrial = {
     type: 'html-button-response',
     stimulus: function () {
@@ -166,83 +194,109 @@ function accept_trial(params, stage, cause=false) {
     timeline: (cause ? [UGtrial, accept_cause, accept_confidence] : [UGtrial, accept_feedback])
   };
 }
-if (learnTrials > 0) {
-  let trials = accept_trial(trialParams, stage=1, cause=false);
-  accept_timeline.push(trials);
-  ACCEPT_TRIALS = ACCEPT_TRIALS + trials.timeline_variables.length * trials.timeline.length;
+
+function accept_mcmc_trial(n) {
+	// create parameters for a single trial for each player/chain
+	let params = players.map(function (player) {
+	  arr = Array(mcmcChains);
+	  for (c = 0; c < mcmcChains; c++) {
+			arr[c] = { ...player }; // copy the player Object to avoid mutation
+			arr[c].choices = ["$" + (jStat.beta.sample(player.alpha, player.beta) * stakes).toFixed(2),
+			          				"$" + (jStat.beta.sample(player.alpha, player.beta) * stakes).toFixed(2)];
+			arr[c].chain = c+1;
+	  }
+	  return arr;
+	}).flat();
+
+	return {
+	  timeline_variables: params,
+	  repetitions: n,
+	  randomize_order: true,
+	  data: {stage: 2, measure: "mcmc"},
+	  timeline: [{
+	    type: 'html-button-response',
+	    stimulus: function () {
+	      return avatar() + "<p>Which offer is " +
+	        jsPsych.timelineVariable('player', true) +
+	        " more likely to make?</p>"
+	    },
+	    choices: function () {
+	      let c = jsPsych.timelineVariable('choices', true);
+	      lastTrial = jsPsych.data.get().filter({
+					stage: 2,
+	        player: jsPsych.timelineVariable('player', true),
+	        chain: jsPsych.timelineVariable('chain', true)
+	      }).last(1).values()[0];
+
+	      // If possible, use the last response as the Markov state
+	      if (lastTrial != undefined) {
+	        c[0] = lastTrial.response;
+	      }
+
+	      // Sample the alternative choice using a proposal distribution
+	      c[1] = "$" + sampleOffer(c[0]).toFixed(2);
+	      while (c[1] == c[0]) {
+	        c[1] = "$" + sampleOffer(c[0]).toFixed(2);
+	      }
+
+	      return [...jsPsych.randomization.shuffle(c)];
+	    },
+	    trial_duration: TRIAL_DURATION,
+	    post_trial_gap: POST_TRIAL_GAP,
+	    button_html: LR_BUTTONS,
+	    on_start: ALLOW_KEYPRESS,
+	    on_finish: function(data) {
+	      DISABLE_KEYPRESS();
+	      data.choices = jsPsych.currentTrial().choices;
+	      data.response = data.choices[data.button_pressed];
+	      data.player = jsPsych.timelineVariable('player', true);
+	      data.trait = jsPsych.timelineVariable('trait', true);
+	      data.alpha = jsPsych.timelineVariable('alpha', true);
+	      data.beta = jsPsych.timelineVariable('beta', true);
+	      data.chain = jsPsych.timelineVariable('chain', true);
+	    }
+	  }]
+	};
 }
 
 
-// Instructions for MCMC section
-var instructions_mcmc_accept = {
-  type: 'instructions',
-  pages: ["<p>Great work!</p><br><p>Now, we want to learn what you think of the different players in this game.</p>",
-          "<p>To do so, we will first tell you which partner you are playing with.</p>" +
-          "<p>Then you'll be shown two possible offers, and you will be asked:</p><br><p>\"Which offer is more likely?\"</p><br>" +
-          "<p><b>Please choose the offer amount that your partner is most likely to have offered.</b></p>",
-          "<p>Sometimes it may seem like both offers are equally likely. " +
-          "Even if it's hard to tell, try to pick the offer that feels most likely. " +
-          "You do not need to spend too much time thinking about each choice.</p>"],
-  show_clickable_nav: true
-};
-if (!TEST) {
-  accept_timeline.push(instructions_mcmc_accept);
-  ACCEPT_TRIALS = ACCEPT_TRIALS + 1;
+if (learnBlocks == 1) {
+	// add learning trials all at once
+	if (learnTrials > 0) {
+	  let trials = accept_learn_trial(learnTrials, stage=1, cause=false);
+	  accept_timeline.push(trials);
+	  ACCEPT_TRIALS = ACCEPT_TRIALS + trials.timeline_variables.length * trials.timeline.length;
+	}
+
+	// Instructions for MCMC section
+	if (!TEST) {
+	  accept_timeline.push(instructions_mcmc_accept);
+	  ACCEPT_TRIALS = ACCEPT_TRIALS + 1;
+	}
+
+	// add MCMC trials all at once
+	if (mcmcTrials > 0) {
+		let trials = accept_mcmc_trial(mcmcTrials);
+	  accept_timeline.push(trials);
+	  ACCEPT_TRIALS = ACCEPT_TRIALS + trials.repetitions * trials.timeline_variables.length;
+	}
+} else if (learnBlocks > 1) {
+	for (i=0; i<learnBlocks; i++) {
+		// add learning trials for this block
+		let learn_trials = accept_trial(learnBlockTrials, stage=1, cause=false);
+		accept_timeline.push(learn_trials);
+		ACCEPT_TRIALS = ACCEPT_TRIALS + learn_trials.timeline_variables.length * learn_trials.timeline.length;
+
+		// add mcmc trials for this block
+		let mcmc_trials = accept_mcmc_trial(mcmcBlockTrials);
+	  accept_timeline.push(mcmc_trials);
+	  ACCEPT_TRIALS = ACCEPT_TRIALS + mcmc_trials.repetitions * mcmc_trials.timeline_variables.length;
+  }
 }
 
-// MCMC trials
-var trials_mcmc_accept = {
-  timeline_variables: mcmcParams,
-  repetitions: mcmcTrials,
-  randomize_order: true,
-  data: {stage: 2, measure: "mcmc"},
-  timeline: [{
-    type: 'html-button-response',
-    stimulus: function () {
-      return avatar() + "<p>Which offer is " +
-        jsPsych.timelineVariable('player', true) +
-        " more likely to make?</p>"
-    },
-    choices: function () {
-      let c = jsPsych.timelineVariable('choices', true);
-      lastTrial = jsPsych.data.get().filter({
-        player: jsPsych.timelineVariable('player', true),
-        chain: jsPsych.timelineVariable('chain', true)
-      }).last(1).values()[0];
 
-      // If possible, use the last response as the Markov state
-      if (lastTrial != undefined) {
-        c[0] = lastTrial.response;
-      }
 
-      // Sample the alternative choice using a proposal distribution
-      c[1] = "$" + sampleOffer(c[0]).toFixed(2);
-      while (c[1] == c[0]) {
-        c[1] = "$" + sampleOffer(c[0]).toFixed(2);
-      }
-      c = jsPsych.randomization.shuffle(c);
-      return [...c];
-    },
-    trial_duration: TRIAL_DURATION,
-    post_trial_gap: POST_TRIAL_GAP,
-    button_html: LR_BUTTONS,
-    on_start: ALLOW_KEYPRESS,
-    on_finish: function(data) {
-      DISABLE_KEYPRESS();
-      data.choices = jsPsych.currentTrial().choices;
-      data.response = data.choices[data.button_pressed];
-      data.player = jsPsych.timelineVariable('player', true);
-      data.trait = jsPsych.timelineVariable('trait', true);
-      data.alpha = jsPsych.timelineVariable('alpha', true);
-      data.beta = jsPsych.timelineVariable('beta', true);
-      data.chain = jsPsych.timelineVariable('chain', true);
-    }
-  }]
-};
-if (mcmcTrials > 0) {
-  accept_timeline.push(trials_mcmc_accept);
-  ACCEPT_TRIALS = ACCEPT_TRIALS + mcmcTrials * mcmcParams.length;
-}
+
 
 // Instructions for test section
 var instructions_test_accept = {
@@ -261,6 +315,14 @@ if (!TEST) {
 
 // test trials
 if (testTrials > 0) {
+  let testParams = players.map(function (player) {
+    return [...Array(testTrials).keys()].map(function (i) {
+			let o = { ...player };
+			o.offer = (stakes * (i / (testTrials-1))).toFixed(2);
+			return o;
+    });
+  }).flat();
+
   let trials = accept_trial(testParams, stage=3, cause=true);
   accept_timeline.push(trials);
   ACCEPT_TRIALS = ACCEPT_TRIALS + trials.timeline_variables.length * trials.timeline.length;
--- exp/exp-propose.js
+++ exp/exp-propose.js
@@ -3,27 +3,57 @@ var propose_timeline = [];
 var PROPOSE_TRIALS = 0;
 
 /* Display instructions trials */
+var instructions_pages_propose = [
+	"<p>Please pay attention and carefully read the following instructions.</p><p>In this HIT, you are going to play a game. " +
+	"There are two players: you, and a partner. In every round of this game, you and your partner receive " + stakes_txt + " ($" + stakes.toFixed(2) +
+	"), and you must decide how to split it between the two of you.</p><br><p>Here's how it works:</p>",
+	"<p>First, you will offer your partner some portion of the money.</p><p>You can offer them any amount between $0.00 and $" + stakes.toFixed(2) + ".</p>",
+	"<p>Once you make an offer, your partner may either accept or reject this offer.</p>" +
+	"<p>If they accept the offer, then they will receive the amount of money you offered to them, and you will get the rest. " +
+	"All of the money you earn through this game will be provided as a bonus after you complete the survey.</p>" +
+	"<p>If you reject the offer, then nobody gets any money in that round.</p>",
+	"<img src='img/red_player.png' height='" + stim_size + "'>" +
+	"<img src='img/yellow_player.png' height='" + stim_size + "'>" +
+	"<img src='img/blue_player.png' height='" + stim_size + "'>" +
+	"<br><p>In each round of the game, you will play with one of three partners: Red, Yellow, or Blue. " +
+	"Before each round, you will be told which partner you are playing with.</p>"];
+
+"<p>Great work!</p><br><p>Now, we want to learn what you think of the different players in this game.</p>",
+"<p>To do so, we will tell you which partner you are playing with, and how much you have offered them.</p>" +
+"<p>Then you will be asked:</p><br><p>\"How likely is this player to accept this offer?\"</p><br>" +
+"<p>Please determine how likely it is for your partner to accept that offer.</p>"
+
+if (learnBlocks > 1) {
+	instructions_pages_propose.push("<p>One last thing: we want to learn what you think of the different players in this game. " +
+		"So, we'll alternate between playing the game for a few rounds and asking what you think of the players.</p>",
+    "<p>To do so, we will tell you which partner you are playing with, and how much you have offered them.</p>" +
+    "<p>Then you will be asked:</p><br><p>\"How likely is this player to accept this offer?\"</p><br>" +
+    "<p><b>Please determine how likely it is for your partner to accept that offer.</b></p>",
+    "<p>Sometimes it may seem hard to say how likely a player is to accept an offer. " +
+    "Even if it's hard to tell, try to pick the number that feels most likely. " +
+    "You do not need to spend too much time thinking about each choice.</p>");
+}
+
 var instructions_propose = {
   type: 'instructions',
   show_clickable_nav: true,
-  pages: ["<p>Please pay attention and carefully read the following instructions.</p><p>In this HIT, you are going to play a game. " +
-          "There are two players: you, and a partner. In every round of this game, you and your partner receive " + stakes_txt + " ($" + stakes.toFixed(2) +
-          "), and you must decide how to split it between the two of you.</p><br><p>Here's how it works:</p>",
-          "<p>First, you will offer your partner some portion of the money.</p><p>You can offer them any amount between $0.00 and $" + stakes.toFixed(2) + ".</p>",
-          "<p>Once you make an offer, your partner may either accept or reject this offer.</p>" +
-          "<p>If they accept the offer, then they will receive the amount of money you offered to them, and you will get the rest. " +
-          "All of the money you earn through this game will be provided as a bonus after you complete the survey.</p>" +
-          "<p>If you reject the offer, then nobody gets any money in that round.</p>",
-          "<img src='img/red_player.png' height='" + stim_size + "'>" +
-          "<img src='img/yellow_player.png' height='" + stim_size + "'>" +
-          "<img src='img/blue_player.png' height='" + stim_size + "'>" +
-          "<br><p>One more thing about this game: in each round of the game, you will play with one of three partners: Red, Yellow, or Blue. " +
-          "Before each round, you will be told which partner you are playing with.</p>"]
+  pages: instructions_pages_propose.concat("You're now finished with the instructions.")
 };
 if (!TEST) {
   propose_timeline.push(instructions_propose);
   PROPOSE_TRIALS = PROPOSE_TRIALS + 1;
 }
+
+/* Instructions for MCMC section */
+var instructions_mcmc_propose = {
+  type: 'instructions',
+  show_clickable_nav: true,
+  pages: ["<p>Great work!</p><br><p>Now, we want to learn what you think of the different players in this game.</p>",
+          "<p>To do so, we will tell you which partner you are playing with, and how much you have offered them.</p>" +
+          "<p>Then you will be asked:</p><br><p>\"How likely is this player to accept this offer?\"</p><br>" +
+          "<p>Please determine how likely it is for your partner to accept that offer.</p>"
+   ]
+};
 
 /* Display check questions and loop until correct */
 var check_choices = ["Your partner gets the amount of money specified in the offer, but you get nothing.",
@@ -110,7 +140,9 @@ var propose_feedback = {
   }
 };
 
-function propose_trial(params, stage, cause=false) {
+function propose_trial(n, stage, cause=false) {
+	let params = learnParams(n);
+
   let UGtrial = {
     type: 'html-slider-response',
     stimulus: function () {
@@ -152,59 +184,77 @@ function propose_trial(params, stage, cause=false) {
   };
 }
 
-/* learning trials */
-if (learnTrials > 0) {
-  let trials = propose_trial(trialParams, stage=1, cause=false);
-  propose_timeline.push(trials);
-  PROPOSE_TRIALS = PROPOSE_TRIALS + trials.timeline_variables.length * trials.timeline.length;
-}
-
-/* Instructions for MCMC section */
-var instructions_mcmc_propose = {
-  type: 'instructions',
-  show_clickable_nav: true,
-  pages: ["<p>Great work!</p><br><p>Now, we want to learn what you think of the different players in this game.</p>",
-          "<p>To do so, we will tell you which partner you are playing with, and how much you have offered them.</p>" +
-          "<p>Then you will be asked:</p><br><p>\"How likely is this player to accept this offer?\"</p><br>" +
-          "<p>Please determine how likely it is for your partner to accept that offer.</p>"
-   ]
-};
-if (!TEST) {
-  propose_timeline.push(instructions_mcmc_propose);
-  PROPOSE_TRIALS = PROPOSE_TRIALS + 1;
-}
 
 var choices;  // use this var to keep track of the choices on every MCMC trial
-
 /* MCMC trials */
-var trials_mcmc_propose = {
-  labels: ["not at all", "totally"],
-  timeline_variables: mcmcParams,
-  trial_duration: TRIAL_DURATION,
-  randomize_order: true,
-  data: {stage: 2, measure: "mcmc"},
-  timeline: [{
-  	type: 'html-slider-response',
-  	stimulus: function () {
-      return avatar() + "<p>Suppose you have made an offer of $" +
-    		jsPsych.timelineVariable('offer', true).toFixed(2) +
-    		" to " + jsPsych.timelineVariable('player', true) +
-    		".</p><p>How likely are they to accept this offer?</p><br>"
-  	},
-  	on_finish: function(data) {
-  	    data.offer = jsPsych.timelineVariable('offer', true);
-  	    data.player = jsPsych.timelineVariable('player', true);
-  	    data.trait = jsPsych.timelineVariable('trait', true);
-  	    data.alpha = jsPsych.timelineVariable('alpha', true);
-  	    data.beta = jsPsych.timelineVariable('beta', true);
-  	}
-  }]
-};
-if (mcmcTrials.length > 0) {
-  propose_timeline.push(trials_mcmc_propose);
-  PROPOSE_TRIALS = PROPOSE_TRIALS + trials_mcmc_propose.length;
+function propose_mcmc_trial(n) {
+	let params = players.map(function (player) {
+    return [...Array(n * mcmcChains).keys()].map(function (i) {
+      let o = { ...player };
+      o.offer = (stakes * (i / (n*mcmcChains-1))).toFixed(2);
+      return o;
+    });
+  }).flat();
+
+	return {
+	  labels: ["not at all", "totally"],
+	  timeline_variables: params,
+	  trial_duration: TRIAL_DURATION,
+	  randomize_order: true,
+	  data: {stage: 2, measure: "mcmc"},
+	  timeline: [{
+	  	type: 'html-slider-response',
+	  	stimulus: function () {
+	      return avatar() + "<p>Suppose you have made an offer of $" +
+	    		jsPsych.timelineVariable('offer', true).toFixed(2) +
+	    		" to " + jsPsych.timelineVariable('player', true) +
+	    		".</p><p>How likely are they to accept this offer?</p><br>"
+	  	},
+	  	on_finish: function(data) {
+	  	    data.offer = jsPsych.timelineVariable('offer', true);
+	  	    data.player = jsPsych.timelineVariable('player', true);
+	  	    data.trait = jsPsych.timelineVariable('trait', true);
+	  	    data.alpha = jsPsych.timelineVariable('alpha', true);
+	  	    data.beta = jsPsych.timelineVariable('beta', true);
+	  	}
+	  }]
+	};
 }
 
+
+if (learnBlocks == 1) {
+	// add learning trials all at once
+	if (learnTrials > 0) {
+	  let trials = propose_learn_trial(learnTrials, stage=1, cause=false);
+	  propose_timeline.push(trials);
+	  PROPOSE_TRIALS = PROPOSE_TRIALS + trials.timeline_variables.length * trials.timeline.length;
+	}
+
+	// Instructions for MCMC section
+	if (!TEST) {
+	  propose_timeline.push(instructions_mcmc_propose);
+	  PROPOSE_TRIALS = PROPOSE_TRIALS + 1;
+	}
+
+	// add MCMC trials all at once
+	if (mcmcTrials.length > 0) {
+		let trials = propose_mcmc_trial(mcmcTrials);
+	  propose_timeline.push(trials);
+	  PROPOSE_TRIALS = PROPOSE_TRIALS + trials.timeline_variables.length;
+	}
+} else if (learnBlocks > 1) {
+	for (i=0; i<learnBlocks; i++) {
+		// add learning trials for this block
+		let learn_trials = propose_learn_trial(learnBlockTrials, stage=1, cause=false);
+	  propose_timeline.push(learn_trials);
+	  PROPOSE_TRIALS = PROPOSE_TRIALS + learn_trials.timeline_variables.length * learn_trials.timeline.length;
+
+		// add mcmc trials for this block
+		let mcmc_trials = propose_mcmc_trial(mcmcBlockTrials);
+	  propose_timeline.push(mcmc_trials);
+	  PROPOSE_TRIALS = PROPOSE_TRIALS + mcmc_trials.timeline_variables.length;
+  }
+}
 
 // Instructions for test section
 var instructions_test_propose = {
@@ -222,7 +272,8 @@ if (!TEST) {
 
 // test trials
 if (testTrials > 0) {
-  let trials = propose_trial(testParams, stage=3, cause=true);
+	// idk what to do in test for propose condition
+  let trials = propose_trial(testTrials, stage=3, cause=true);
   propose_timeline.push(trials);
   PROPOSE_TRIALS = PROPOSE_TRIALS + trials.timeline_variables.length * trials.timeline.length;
 }
